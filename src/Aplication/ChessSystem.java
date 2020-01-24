45a12701375c3ba0786a1c929dbec972d402a5a8
FrostPriest <johnericjahn@gmail.com>
2020-01-24 05:07:01 -0300

    First Class: Position

--- src/Aplication/ChessSystem.java
+++ src/Aplication/ChessSystem.java
@@ -1,9 +1,12 @@
 package Aplication;
 
+import BoardGame.Position;
+
 public class ChessSystem {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
     
 }
